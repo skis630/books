ee86c69e0e57039d92370f4a65d48e82d745e5bc
sarakisch <sara.k@fbx-group.com>
2021-08-23 02:04:16 +0300

    Add route to single book

--- be/routes/books.js
+++ be/routes/books.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const books = require('../services/books');
 
 /* GET books. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
-    console.log(process.env.DB_USER, process.env.DB_PASSWORD);
     res.json(await books.getBooks());
   } catch (err) {
     console.error(`Error while loading books `, err.message);
     next(err);
   }
 });
+
+// Get one book
+router.get('/:bookId', async function (req, res, next) {
+  try {
+    res.json(await books.getBook(req.params.bookId));
+  } catch (err) {
+    console.error(`Error while loading books `, err.message);
+    next(err);
+  }
+})
 
 module.exports = router;
